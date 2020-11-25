502db844768c8926eb6166a2af3acc4e2c2820d1
lagofelipe <lagofelipe@gmail.com>
2020-11-25 11:58:43 -0600

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-// write your code here
+
+
+//once content is loaded fectch
+document.addEventListener('DOMContentLoaded', ()=> {
+    fetchImages();
+  });
+
+  function fetchImages () {
+    fetch('http://localhost:3000/images/1')
+      .then(response => response.json())
+      .then(image => renderMenu(image))
+     };
+
+
+
+//create image
+
+  function renderMenu(image){
+      //console.log(data)
+
+
+    for (const obj of image){
+        const imageContainer = document.querySelector('.image-container')
+        const immageCard = document.querySelector('.image-card')
+
+        const imgLi = document.createElement('li')
+        const img = document.createElement('img')
+        const h2 = document.createElement('h2')
+        h2.innerHTML = image.title
+        console.log(image.title)
+    }
+
+}
+
+// const imageContainer = document.querySelector('.image-container')
+// data.ForEach(image => {
+//  const imgLi = document.createElement('li')
+
+//  const img = document.createElement('img')
+//  img.setAttribute('src', image.image)
